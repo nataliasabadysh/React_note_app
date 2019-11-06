46aefde41c94a37632bfcb8a0a27fe6f47f537a6
nataliasabadysh <nataliasabadysh@gmail.com>
2019-11-06 20:58:26 +0200

    Created new app with files structure and dependencies

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// Core
+import React from 'react';
+// Components
+import Notes from './components/Notess';
+// Instruments
+import './App.css';
+
+const App = () => <Notes />
+
+export default App;
